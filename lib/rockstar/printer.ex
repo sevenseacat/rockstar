853623011135abd214ca993d870b2f86edeb3798
Rebecca Le <traybaby@gmail.com>
2018-10-27 21:49:16 +1100

    Convert assignment AST tuples to valid Elixir code

--- lib/rockstar/printer.ex
+++ lib/rockstar/printer.ex
@@ -4,8 +4,27 @@ defmodule Rockstar.Printer do
 
   iex> Printer.print({:comment, "Hello world!"})
   "# Hello world!"
+
+  iex> Printer.print({:assign, "my world", "something cool"})
+  "my_world = \\"something cool\\""
+
+  iex> Printer.print({:assign, "Sally", 12})
+  "sally = 12"
   """
   def print({:comment, comment}), do: "# " <> comment
 
+  def print({:assign, variable, value}) do
+    string_to_variable_name(variable) <> " = #{wrap_string(value)}"
+  end
+
   def print(line), do: line
+
+  defp string_to_variable_name(name) do
+    name
+    |> String.replace(" ", "_")
+    |> String.downcase()
+  end
+
+  defp wrap_string(value) when is_binary(value), do: "\"" <> value <> "\""
+  defp wrap_string(value), do: value
 end
